09800279b431c5ae43c70ac4c43bc1854394c4a2
waleedza99 <35402892+waleedza99@users.noreply.github.com>
2022-10-24 22:57:00 -0400

    Initial commit

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,30 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: '/fakeApi' }),
+  tagTypes: ['Post'],
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => '/posts',
+      providesTags: ['Post'],
+    }),
+    getPost: builder.query({
+      query: (postId) => `/posts/${postId}`,
+    }),
+    addNewPost: builder.mutation({
+      query: (initialPost) => ({
+        url: '/posts',
+        method: 'POST',
+        body: initialPost,
+      }),
+      invalidatesTags: ['Post'],
+    }),
+  }),
+})
+
+export const {
+  useGetPostsQuery,
+  useGetPostQuery,
+  useAddNewPostMutation,
+} = apiSlice
